408bc832b44a32db9bfb93ace40a836e879cf3e3
Michle99 <sammiyonyx@gmail.com>
2024-01-27 19:44:01 -0500

    refactored `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,7 @@
-import React, { useState } from 'react';
-import TodoList from './modules/TodoList/components/TodoList'
-import TodoForm from './modules/TodoForm/components/TodoForm';
+import React from 'react';
 import { Container, CssBaseline, Paper, Box, Typography } from '@mui/material';
 
-
-
 const App: React.FC = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const handleAddTodo = (text: string) => {
-    const newTodo = { id: Date.now().toString(), text, completed: false };
-    setTodos((prevTodos) => [...prevTodos, newTodo]);
-  };
-
-  const handleDeleteTodo = (id: string) => {
-    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
-  };
-
-  const handleToggleTodo = (id: string) => {
-    setTodos((prevTodos) =>
-      prevTodos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
-  };
 
   return (
     <Container component="main" maxWidth="md">
@@ -33,8 +11,6 @@ const App: React.FC = () => {
           <Typography component="h1" variant="h4">
             Micro Frontend Todo App
           </Typography>
-          <TodoForm onAdd={handleAddTodo} />
-          <TodoList todos={todos} onDelete={handleDeleteTodo} onToggle={handleToggleTodo} />
         </Box>
       </Paper>
     </Container>
