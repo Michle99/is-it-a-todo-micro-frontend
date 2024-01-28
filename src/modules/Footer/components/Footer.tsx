acf8cae94c768be85f2264e74ab55e9a77763d1f
Michle99 <sammiyonyx@gmail.com>
2024-01-27 19:47:19 -0500

    added `Footer` comp

---
+++ src/modules/Footer/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+const Footer: React.FC = () => {
+  return (
+    <Paper elevation={3} style={{ position: 'fixed', bottom: 0, width: '100%' }}>
+      <BottomNavigation showLabels>
+        <BottomNavigationAction 
+            label="GitHub" 
+            icon={<GitHubIcon />} 
+            href="https://github.com/Michle88/todo-microfrontend" 
+        />
+      </BottomNavigation>
+    </Paper>
+  );
+};
+
+export default Footer;
