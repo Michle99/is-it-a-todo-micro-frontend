782d9014d9d3df3f1cc42c54b8ef3cab5a8df7cb
Michle99 <sammiyonyx@gmail.com>
2024-01-27 19:40:12 -0500

    updated `TodoApp` with respective Todo comps

---
+++ src/modules/TodoApp/component/TodoApp.tsx
@@ -0,0 +1,38 @@
+import { Container, CssBaseline, Paper, Typography } from "@mui/material";
+import { useState } from "react";
+import TodoList from "../../TodoList/components/TodoList";
+import TodoForm from "../../TodoForm/components/TodoForm";
+
+
+const TodoApp: React.FC = () => {
+    const [todos, setTodos] = useState<{ id: string; text: string; completed: boolean }[]>([]);
+  
+    const handleAddTodo = (text: string) => {
+      setTodos((prevTodos) => [...prevTodos, { id: Date.now().toString(), text, completed: false }]);
+    };
+  
+    const handleToggleTodo = (id: string) => {
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) => (todo.id === id ? { ...todo, completed: !todo.completed } : todo))
+      );
+    };
+  
+    const handleDeleteTodo = (id: string) => {
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+    };
+  
+    return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Paper elevation={3} style={{ padding: '20px', margin: '20px 0' }}>
+          <Typography component="h1" variant="h5">
+            Todo App
+          </Typography>
+          <TodoForm onAdd={handleAddTodo} />
+          <TodoList todos={todos} onToggle={handleToggleTodo} onDelete={handleDeleteTodo} />
+        </Paper>
+      </Container>
+    );
+};
+  
+export default TodoApp
