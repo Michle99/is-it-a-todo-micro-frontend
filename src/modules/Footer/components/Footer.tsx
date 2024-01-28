30d6f567ecbbbaa10912601ce74079be9b741a45
Michle99 <sammiyonyx@gmail.com>
2024-01-28 06:12:44 -0500

    updated `Footer`

--- src/modules/Footer/components/Footer.tsx
+++ src/modules/Footer/components/Footer.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { BottomNavigation, BottomNavigationAction, Paper } from '@mui/material';
+import { BottomNavigation, BottomNavigationAction, Box, Paper, Typography } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Footer: React.FC = () => {
   return (
     <Paper elevation={3} style={{ position: 'fixed', bottom: 0, width: '100%' }}>
       <BottomNavigation showLabels>
-        <BottomNavigationAction 
-            label="GitHub" 
-            icon={<GitHubIcon />} 
-            href="https://github.com/Michle99/todo-microfrontend" 
-        />
+        <Box display="flex" justifyContent="space-between" alignItems="center" px={2}>
+            <Typography variant="body2" color="textSecondary">
+              &copy; {new Date().getFullYear()} Todo App
+            </Typography>
+            <BottomNavigationAction 
+              label="GitHub" 
+              icon={<GitHubIcon />} 
+              href="https://github.com/Michle99/todo-microfrontend" 
+            />
+          </Box>
       </BottomNavigation>
     </Paper>
   );
